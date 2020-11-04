d0da568ed15b9d3d7946153d0a8f87175645fa01
LNgroff <LNgroff@gmail.com>
2020-11-04 23:45:32 +0000

    created and added beginning to server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+"""Server for park finder app."""
+
+from flask import (Flask, render_template, request, flash, session,
+                    redirect)
+from model import connect_to_db
+import crud
+import os
+
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def get_homepage():
+    """View homepage."""
+
+    return render_template('homepage.html')
