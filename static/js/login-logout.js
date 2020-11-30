77f73902ce8526b6c25287fd6ea8099802bd15cb
LNgroff <LNgroff@gmail.com>
2020-11-30 06:31:31 +0000

    implemented drop down menu

--- static/js/login-logout.js
+++ static/js/login-logout.js
@@ -1,56 +1,15 @@
 "use sctrict";
 
 
-$.get('/loggedin', (res) => {
+$(document).ready(function() {
 
-    if (res === true) {
-        $('#login-button').click(function(){
-            $('#login-out').attr('action', '/logout');
-        });
-    };
+    $('#login').hide();
+
+    $('#show-login').on({
+        click: function(){
+            $('#login').toggle();
+
+        }
+    });
+
 });
-
-// function logInOut(evt) {
-//     // if the button already says 'log-in':
-//     if ($('#login-button').text()=== 'Log in') {
-//         $('login-out').setAttribute("action", "/logout");
-//         $('#login-button').html('Log out');
-//         // change it to log-out
-
-//     } else {
-// ;       $('#login-button').html('Log in')
-//         $('login-out').action('/login');}
-//     // otherwise (if it says 'log-out') change it back to 'log-in'
-// };
-
-// // on the action of clicking the button, fun the logInOut function 
-// $('#login-button').on('click', (logInOut));
-
-// const navLogin = document.getElementById("login-button");
-// const loginForm = document.getElementById("login-out");
-
-
-// $.get('/loggedin', (res) => {
-
-//     if (res === true) {
-//         navLogin.innerHTML = "Log out";
-//         loginForm.setAttribute("action", "/logout");
-//     };
-// });
-
-// $('#login-button').on('click', (logInOut));
-
-
-// $("#login-button").click(function(){
-//     $("#login-out").att("action"),
-// }) 
-
-// $(document).ready(function(){
-//     $('#login-trigger').click(function(){
-//       $(this).next('#login-content').slideToggle();
-//       $(this).toggleClass('active');
-
-//       if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
-//         else $(this).find('span').html('&#x25BC;')
-//       })
-//   });
