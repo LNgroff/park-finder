8313c28f592c14425680184c0810a5dccf3f773d
LNgroff <LNgroff@gmail.com>
2020-12-01 23:11:17 +0000

    psuedo code for failed modal

--- static/js/image-gallery.js
+++ static/js/image-gallery.js
@@ -1,74 +1,71 @@
+"use strict";
 
-// $('img').on('click', function(e) {
-//     $('#imgViewer').html('').append( $(e.currentTarget).clone().removeClass('img-responsive').removeClass('img-thumbnail') )
-//     $('#viewImg').modal('show')
-// })
 
-// $(document).ready(function () {
-//     $('#image').on('click', function () {
-//         (this).width($(this).width() * 2);
+// function toggleExpand() {
+//     var expanded = true;
+//     $('img').each(function() {
+//         if (!$(this).hasClass('expanded')) {
+//             expanded = false;
+//             return false;
+//         }
 //     });
-// })
-
-
-// const displayedImage = document.querySelector('.displayed-img');
-// const thumbBar = document.querySelector('.thumb-bar');
-
-// const btn = document.querySelector('button');
-// const overlay = document.querySelector('.overlay');
-
-// /* Looping through images */
-// let images = document.getElementById("gallery-view").value;
-
-
-// for(let i=0;i<5;i++) {
-//     const newImage = document.createElement('img');
-//     newImage.setAttribute('src', images[i].image_url);
-//     thumbBar.appendChild(newImage);
-//     newImage.onclick=function(e){
-//     displayedImage.src=e.target.src;
-//     }
+//     $('a[href$=".png"], a[href$=".jpg"], a[href$=".gif"], a[href$=".bmp"]').children('img').each(function() {
+//         if (expanded)
+//             $(this).removeClass('expanded');
+//         else
+//             $(this).addClass('expanded');
+//     });
 // }
 
-//     /* Wiring up the Darken/Lighten button */
-// btn.onclick=function(){
-//     if (btn.classList.contains('dark')) {
-//         btn.classList.add('light');
-//         btn.textContent = 'Lighten';
-//         overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-//         btn.classList.remove('dark');
-//     } else {
-//         btn.classList.add('dark');
-//         btn.textContent = 'Darken';
-//         overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-//     }
-// });
-// $(document).ready(function() {
-//     var imageLinks = $('a[href$=".png"], a[href$=".jpg"], a[href$=".gif"], a[href$=".bmp"]');
-//     if (imageLinks.children('img').length) {
-//         imageLinks.children('img').each(function() {
-//             var currentTitle = $(this).attr('title');
-//             $(this).attr('title', currentTitle + ' (click to enlarge image)');
-//         });
-//         imageLinks.click(function(e) {
-//             e.preventDefault();
-//             $(this).children('img').toggleClass('expanded');
-//         });
-//     }
+// $(function() {
+//     $('.pop').on('click', function() {
+//         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+//         $('#imagemodal').modal('toggle');   
+//     });		
 // });
 
-function toggleExpand() {
-    var expanded = true;
-    $('img').each(function() {
-        if (!$(this).hasClass('expanded')) {
-            expanded = false;
-            return false;
-        }
-    });
-    $('a[href$=".png"], a[href$=".jpg"], a[href$=".gif"], a[href$=".bmp"]').children('img').each(function() {
-        if (expanded)
-            $(this).removeClass('expanded');
-        else
-            $(this).addClass('expanded');
-    });
-}
+// $(".pop").on("click", function(e) {
+//     // e.preventDefault() this is stopping the redirect to the image its self
+//     e.preventDefault();
+//     // #the-modal is the img tag that I use as the modal.
+//     $('#imagemodal').modal('toggle');
+// });
+
+// $(".pop").on("click", function(e) {
+//     // e.preventDefault() this is stopping the redirect to the image its self
+//     e.preventDefault();
+//     // #the-modal is the img tag that I use as the modal.
+//     $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+//     $('#imagemodal').modal('toggle');
+// });
+
+// $(".pop").on("click", function(e) {
+//     e.preventDefault();
+//     $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+//     $('#imagemodal').modal('toggle');
+//     $('#imagemodal').modal('show');
+//     $('#imagemodal').modal({
+//     keyboard: false
+//     });
+
+// });
+
+// Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById("myImg");
+// var modalImg = document.getElementById("img01");
+
+// img.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
